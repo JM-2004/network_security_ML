fb473245f291ac591c6bcf37f9e3bae9ad20546d
JM-2004 <jyoti.231ee122@nitk.edu.in>
2025-07-08 17:21:58 +0530

    minor change

--- app.py
+++ app.py
@@ -89,5 +89,7 @@ async def predict_route(request: Request,file: UploadFile = File(...)):
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
     
-if __name__=="__main__":
-    app_run(app,host="0.0.0.0",port=8000)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    app_run(app, host="0.0.0.0", port=port)
